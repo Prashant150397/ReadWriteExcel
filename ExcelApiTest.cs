5e431c8ca17083c8b8bc3303d7cb8d84d7effa21
prashantkr1223 <prashant.rajput@kiwiqa.com>
2023-04-09 20:47:16 +0530

    Add project files.

---
+++ ExcelApiTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using xl = Microsoft.Office.Interop.Excel;
+
+namespace ReadWriteExcel
+{
+     class ExcelApiTest
+    {
+        xl.Application xlApp = null;
+        xl.Workbook workbook = null;
+        xl.Workbooks workbooks = null;
+        Hashtable sheets;
+        public string xlFilePath;
+
+        public ExcelApiTest(string xlFilePath)
+            {
+             this.xlFilePath = xlFilePath;
+            }
+
+        public void openExcel()
+        {
+            xlApp = new xl.Application();
+            workbooks =xlApp.Workbooks;
+            workbook = workbooks.Open(xlFilePath);
+            sheets=new Hashtable();
+            int count = 1;
+            foreach (xl.Worksheet sheet in workbook.Sheets)
+            {
+                sheets[count]=sheet.Name;
+                count++;
+            }
+        }
+        public void closeExcel() 
+        {
+            workbook.Close(false,xlFilePath,null);
+            Marshal.FinalReleaseComObject(xlApp);
+           // workbook = null;
+
+
+            workbooks.Close();
+            Marshal.FinalReleaseComObject(workbooks);
+            //workbooks = null;
+
+            xlApp.Quit();
+            Marshal.FinalReleaseComObject(xlApp);
+           // xlApp = null;
+        }
+
+        public int getRowCount(string sheetName)
+        {
+            openExcel();
+
+            int rowCount = 0;
+            int sheetValue = 0;
+
+            if(sheets.ContainsValue(sheetName))
+            {
+                foreach(DictionaryEntry sheet in sheets)
+                {
+                    if(sheet.Value.Equals(sheetName))
+                    {
+                        sheetValue = (int)sheet.Key;
+
+
+                    }
+                }
+                xl.Worksheet worksheet = workbook.Worksheets[sheetValue] as xl.Worksheet;
+                xl.Range range = worksheet.UsedRange;
+                rowCount = range.Rows.Count;
+
+            }
+            closeExcel();
+            return rowCount;
+        }
+    }
+}
---
+++ checkExcel.cs
@@ -0,0 +1,35 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadWriteExcel
+{
+     class checkExcel
+    {
+        static void Main(string[] args)
+        {
+
+
+            int[] arr = { 5,23,19,4, 5, 6, 7 };
+
+            int max = arr[0];
+            int min = arr[0];
+
+            for (int i = 1; i < arr.Length; i++)
+            { 
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }else if (arr[i] < min) { min= arr[i]; }
+            }
+            Console.WriteLine(max+ " max value");
+            Console.WriteLine(min + " min value");
+        }
+        
+        }
+}
